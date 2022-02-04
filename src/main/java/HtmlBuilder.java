4491449f7c48c99ab4608d2ee02eacc951a3aecd
Baptiste <30670643+patrickcjk@users.noreply.github.com>
2022-02-04 14:23:25 +0100

    --

--- src/main/java/HtmlBuilder.java
+++ src/main/java/HtmlBuilder.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class HtmlBuilder {
+
+public class HtmlBuilder {
+    private StringBuilder _stringBuilder = new StringBuilder();
+
+    public HtmlBuilder() {
+
+    }
+
+    /*public void addUnorderedListItem() {
+        _stringBuilder.append("");
+    }
+
+    public void addUnorderedList() {
+        var nestedBuiled = new HtmlBuilder();
+        //nestedBuiled.add
+
+    }*/
+
+    public String toString() {
+        return _stringBuilder.toString();
+    }
+
+    public void append(String str) {
+        _stringBuilder.append(str);
+    }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,31 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
-    public static void main(String[] args) {
-        
+    private static String GetViewByName(String viewName) throws IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream input = classLoader.getResourceAsStream(viewName);
+        return new String(input.readAllBytes(), StandardCharsets.UTF_8);
+    }
+
+    private static String GenerateIndexView(List<Object> viewModel) throws IOException {
+        var indexFile = GetViewByName("index.html");
+
+        var htmlBuilder = new HtmlBuilder();
+        for (var model: viewModel)
+            htmlBuilder.append(String.format("<li>%s</li>", "Hello World"));
+
+        indexFile = indexFile.replace("$elements", htmlBuilder.toString());
+        return indexFile;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        var indexFileContent = GenerateIndexView(new ArrayList<>());
+
     }
 }
