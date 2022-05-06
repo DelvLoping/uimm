8893aa1bdc2c67e672bfd35abb972a5f302d9b3b
Baptiste <b.charlier@estalis.fr>
2022-05-06 10:58:31 +0200

    --

--- src/main/java/MaterialModel.java
+++ src/main/java/MaterialModel.java
@@ -6,11 +6,14 @@ public class MaterialModel {
     public String label;
     public boolean value;
 
-    public MaterialModel(String text, boolean val){
-        label=text;
-        value=val;
+    public MaterialModel(String _label, boolean _value) {
+        label = _label;
+        value = _value;
     }
 
+    /**
+     * Méthode qui retourne le nom du fichier à partir du materiel actuel (supression des caractères spéciaux etc)
+     */
     public String getFileName(String extension) {
         return String.format("%s.%s", label.toLowerCase().replaceAll("[^a-z]+",""), extension);
     }
